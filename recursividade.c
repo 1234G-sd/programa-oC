912f0fca5eabc381da9d1758656be5d51b5cabd6
Gabriel07 <gabrielfilipe15722@gmail.com>
2025-05-26 16:50:42 +0000

    feito e executado com sucesso

---
+++ recursividade.c
@@ -0,0 +1,34 @@
+
+#include <stdio.h>
+
+
+void recursivo (int numero) {
+
+    if(numero > 0){
+     printf("%d\n", numero);
+
+     recursivo(numero -1);
+
+
+    }
+
+    printf("\n");
+
+
+}
+
+
+
+int main() {
+
+int quantidade = 10;
+
+printf("Contagem Regressiva...\n");
+
+
+recursivo(quantidade);
+
+
+
+return 0;
+}
