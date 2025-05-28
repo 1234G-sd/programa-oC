181eafb16e5f24d0dbe977caed53274705efb63b
Gabriel07 <gabrielfilipe15722@gmail.com>
2025-05-28 19:38:26 +0000

    feito e executado com sucesso

---
+++ teste01.c
@@ -0,0 +1,46 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void moverTorre(int casas);
+void moverRainha(int casas);
+
+
+
+void moverTorre(int casas){
+if(casas > 0 ){
+
+printf("Direita...\n");
+
+moverTorre(casas - 1);
+
+}
+
+printf("\n");
+
+}
+
+
+void moverRainha(int casas){
+
+if(casas > 0){
+printf("Esquerda...\n");
+moverRainha(casas - 1);
+}
+}
+
+
+
+
+
+int main() {
+
+printf("TORRE:\n");
+moverTorre(5);
+printf("RAINHA:\n");
+moverRainha(8);
+char nome[20];
+
+
+return 0;    
+}
